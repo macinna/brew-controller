467ff84559c18997ba87a608cb366faa31080e7b
macinna <allan.macinnis@gmail.com>
2015-10-12 19:54:57 -0500

    added stub for wiringPi library so app will run without a raspberry pi

---
+++ wiring-pi-stub.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Allan on 10/11/2015.
+ */
+
+module.exports = {
+    wiringPiSetupGpio : function () { },
+    digitalWrite : function(pin, status) { },
+    pinMode : function(pin, mode){ },
+    LOW : 0,
+    HIGH : 1
+};
